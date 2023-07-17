cb8b8750ae7128320d8da164c57132b0de7d2b5f
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-17 17:29:27 -0500

    :hammer: Return to button type

--- page-checkout.php
+++ page-checkout.php
@@ -5,7 +5,6 @@ $order = filter_input( INPUT_GET, 'order' );
 get_header(); ?>
 
     <div id="main-container">
-		<button id="testing-callback" type="button">PRUEBA DE SIEMPRE</button>
         <div class="content">
 			<?php if ( is_null( $order ) ): ?>
                 <h1 class="checkout-title"><?php the_title(); ?></h1>
