052cc631ee6414742b4849477be433a21ca0eec0
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-17 23:30:56 -0500

    :hammer: Add new containers additional info

--- page-checkout.php
+++ page-checkout.php
@@ -18,6 +18,28 @@ get_header(); ?>
 			}
 			?>
         </div>
+
+		<div class="checkout_additional_info woocommerce-notices-wrapper">
+			<div class="checkout_additional_info__title">
+				Recuerda...
+			</div>
+
+			<div class="checkout_additional_info__element">
+				Tiempo de entrega 2 a 3 días habiles (sin contar Feriados, Sábados y Domingos)
+			</div>
+
+			<div class="checkout_additional_info__element">
+				<a href="https://skullcandy.com.pe/envios/">Envíos a provincia</a>
+			</div>
+
+			<div class="checkout_additional_info__element">
+				<a href="https://skullcandy.com.pe/politica-de-cambios-y-garantias/">
+					<button class="checkout_additional_info__element__btn">Garantías</button>
+				</a>
+			</div>
+
+
+		</div>
     </div>
 
 <?php get_footer();
