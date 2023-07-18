dd5957a9dc7d599a2e21ef9d11c6cdce4e7e96da
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-17 23:09:21 -0500

    :lipstick: Correct checkout coupon

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -104,7 +104,7 @@ defined( 'ABSPATH' ) || exit;
 			<td><?php wc_cart_totals_order_total_html(); ?></td>
 		</tr>
 		<tr>
-			<td>
+			<td colspan="2">
 				<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
 			</td>
 		</tr>
