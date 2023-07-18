2c0d187461dc5ddbe25ae16fb8d7b18ca146a1b0
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-17 23:51:10 -0500

    :hammer: Add new captios to text

--- page-checkout.php
+++ page-checkout.php
@@ -26,18 +26,18 @@ get_header(); ?>
 
 			<div class="checkout_additional_info__content">
 				<div class="checkout_additional_info__element">
-					<p class="checkout_additional_info__element__label">Para Lima y Callao nuestro tiempo de entrega es de 2 a 3 días habiles (sin contar Feriados, Sábados y Domingos).</p>
+					<p class="checkout_additional_info__element__label">El tiempo de entrega es de 2 a 3 días habiles para Lima Metropolitana y Callao (sin contar Feriados, Sábados y Domingos).</p>
 				</div>
 
 				<div class="checkout_additional_info__element">
-					<p class="checkout_additional_info__element__label">Para envíos a provincia es de 4 a 15 días hábiles según la región</p>
+					<p class="checkout_additional_info__element__label">El tiempo de entrega a provincia es de 4 a 15 días hábiles según la región</p>
 					<a href="https://skullcandy.com.pe/envios/">
 						<button class="checkout_additional_info__element__btn">Ver más</button>
 					</a>
 				</div>
 
 				<div class="checkout_additional_info__element">
-					<p class="checkout_additional_info__element__label">Sobre nuestra política de cambios y devoluciones</p>
+					<p class="checkout_additional_info__element__label">Entérate sorbe nuestra política de cambios y devoluciones</p>
 					<a href="https://skullcandy.com.pe/politica-de-cambios-y-garantias/">
 						<button class="checkout_additional_info__element__btn">Ver más</button>
 					</a>
