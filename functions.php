844fb60789b49484bc8e33a0046fea79bdadabe0
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-18 21:32:35 -0500

    :sparkles: Complete functionality of checkout process

--- functions.php
+++ functions.php
@@ -1041,3 +1041,5 @@ return '';
 if ( !function_exists( 'wp_password_change_notification' ) ) {
     function wp_password_change_notification() {}
 }
+
+// COMPLETE FUNCTIONALITY WITHOUTH TYPE OF SHIPMENT
