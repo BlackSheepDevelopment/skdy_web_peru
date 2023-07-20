7b0ded6e1b6fc520974f24e12222a93206ac5352
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-19 22:03:20 -0500

    :new values:

--- extra/woo-variations-select2.js
+++ extra/woo-variations-select2.js
@@ -86,7 +86,7 @@ jQuery(document).ready(function ($) {
     // Step 1: Remove options with class name attribute and data-lc-qty equal to 0
 
     const selectElement = $("#select_location");
-    var options = selectElement.data();
+    var options = selectElement.find("option");
 
     console.log(options);
     // Step 1: Remove options with class name attribute and data-lc-qty equal to 0
