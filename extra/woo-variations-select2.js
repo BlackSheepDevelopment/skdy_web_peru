c061c248fade35875abed157382e3c2516846a29
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-20 00:12:05 -0500

    :alembic: Cal new events

--- extra/woo-variations-select2.js
+++ extra/woo-variations-select2.js
@@ -83,48 +83,47 @@ jQuery(document).ready(function ($) {
     templateResult: locationFormatResult,
     templateSelection: locationFormatSelected,
   });
+});
 
+$(".single_add_to_cart_button").on("click", function (e) {
   console.log("New testing 2");
+  if (!doneShipment) {
+    e.preventDefault();
 
-  $(".single_add_to_cart_button").on("click", function (e) {
-    if (!doneShipment) {
-      e.preventDefault();
+    const selectElement = $("#select_location")[0];
+    const selectOptions = selectElement.options;
+    let desiredShipment = "0";
+    let savarData = selectOptions[1];
+    let primeData = selectOptions[2];
 
-      const selectElement = $("#select_location")[0];
-      const selectOptions = selectElement.options;
-      let desiredShipment = "0";
-      let savarData = selectOptions[1];
-      let primeData = selectOptions[2];
+    console.log(savarData.className);
+    console.log(primeData.className);
 
-      console.log(savarData.className);
-      console.log(primeData.className);
+    console.log(savarData.dataset.lcQty);
+    console.log(primeData.dataset.lcQty);
 
-      console.log(savarData.dataset.lcQty);
-      console.log(primeData.dataset.lcQty);
-
-      if (
-        savarData.className == "wclimloc_savar24" &&
-        savarData.dataset.lcQty &&
-        parseInt(savarData.dataset.lcQty) > 0
-      ) {
-        desiredShipment = "0";
-      } else if (
-        primeData.className == "wclimloc_almacen" &&
-        primeData.dataset.lcQty &&
-        parseInt(primeData.dataset.lcQty) > 0
-      ) {
-        desiredShipment = "1";
-      } else {
-        desiredShipment = "0";
-      }
-
-      console.log("desiredShipment: ");
-      console.log(desiredShipment);
-      $("#select_location").val(desiredShipment);
-      $("#select_location").trigger("change");
-      doneShipment = true;
+    if (
+      savarData.className == "wclimloc_savar24" &&
+      savarData.dataset.lcQty &&
+      parseInt(savarData.dataset.lcQty) > 0
+    ) {
+      desiredShipment = "0";
+    } else if (
+      primeData.className == "wclimloc_almacen" &&
+      primeData.dataset.lcQty &&
+      parseInt(primeData.dataset.lcQty) > 0
+    ) {
+      desiredShipment = "1";
+    } else {
+      desiredShipment = "0";
     }
 
-    $(this).trigger("click");
-  });
+    console.log("desiredShipment: ");
+    console.log(desiredShipment);
+    $("#select_location").val(desiredShipment);
+    $("#select_location").trigger("change");
+    doneShipment = true;
+  }
+
+  $(this).trigger("click");
 });
