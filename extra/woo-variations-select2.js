c87ac75241cca041e971495493735cdcbb6b0fc5
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-19 21:20:31 -0500

    :hammer: Access selected

--- extra/woo-variations-select2.js
+++ extra/woo-variations-select2.js
@@ -82,7 +82,7 @@ jQuery(document).ready(function ($) {
     templateSelection: locationFormatSelected,
   });
 
-  console.log($("#select_location"));
+  console.log($("#select_location")[0]);
 
   $("#select_location").on("select2:select", function (e) {
     console.log("hello");
