b4cb25030829c190b204b8280bd2b9defe8b876f
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-19 20:00:53 -0500

    :hammer: Comment select2 from locations in product page

--- extra/woo-variations-select2.js
+++ extra/woo-variations-select2.js
@@ -1,66 +1,84 @@
 jQuery(document).ready(function ($) {
-    if (window.product_layout === 'layout-1') {
-
-    }
-    switch (window.product_layout) {
-        case 'layout-1':
-
-        function formatResult(state) {
-            if (!state.id) return state.text;
-
-            return $(
-                '<img src="' + window.swatches_images[state.element.index] + '" alt="' + state.text + '" />' +
-                '<span>' + state.text + '</span>'
-            );
-        }
-
-            break;
-    }
-
-    function formatSelected(state) {
+  if (window.product_layout === "layout-1") {
+  }
+  switch (window.product_layout) {
+    case "layout-1":
+      function formatResult(state) {
         if (!state.id) return state.text;
 
-        switch (window.product_layout) {
-            case 'layout-1':
-                $('#product-header .background source').attr('srcset', window.gallery_images[state.element.index]['desktop']);
-                $('#product-header .background img').attr('src', window.gallery_images[state.element.index]['mobile']);
+        return $(
+          '<img src="' +
+            window.swatches_images[state.element.index] +
+            '" alt="' +
+            state.text +
+            '" />' +
+            "<span>" +
+            state.text +
+            "</span>"
+        );
+      }
 
-                return $(
-                    '<img src="' + window.swatches_images[state.element.index] + '" alt="' + state.text + '" />' +
-                    '<span>' + state.text + '</span>'
-                );
-            case 'layout-2':
-                $('.list-swatches .swatch[data-color="' + state.text + '"]').addClass('selected');
-        }
+      break;
+  }
 
-        return $('<span>' + state.text + '</span>');
+  function formatSelected(state) {
+    if (!state.id) return state.text;
+
+    switch (window.product_layout) {
+      case "layout-1":
+        $("#product-header .background source").attr(
+          "srcset",
+          window.gallery_images[state.element.index]["desktop"]
+        );
+        $("#product-header .background img").attr(
+          "src",
+          window.gallery_images[state.element.index]["mobile"]
+        );
+
+        return $(
+          '<img src="' +
+            window.swatches_images[state.element.index] +
+            '" alt="' +
+            state.text +
+            '" />' +
+            "<span>" +
+            state.text +
+            "</span>"
+        );
+      case "layout-2":
+        $('.list-swatches .swatch[data-color="' + state.text + '"]').addClass(
+          "selected"
+        );
     }
 
-    $('#color option').first().remove();
-    $('#color').select2({
-        minimumResultsForSearch: -1,
-        dropdownParent: $('.summary.entry-summary'),
-        templateResult: formatResult,
-        templateSelection: formatSelected
-    });
+    return $("<span>" + state.text + "</span>");
+  }
 
-    function locationFormatResult(state) {
-        if (state.text === ' - Select Location - ') {
-            return ' - Tipo de envío - '
-        }
-        return state.text.replace('Out of Stock', 'Agotado');
+  $("#color option").first().remove();
+  $("#color").select2({
+    minimumResultsForSearch: -1,
+    dropdownParent: $(".summary.entry-summary"),
+    templateResult: formatResult,
+    templateSelection: formatSelected,
+  });
+
+  function locationFormatResult(state) {
+    if (state.text === " - Select Location - ") {
+      return " - Tipo de envío - ";
     }
+    return state.text.replace("Out of Stock", "Agotado");
+  }
 
-    function locationFormatSelected(state) {
-        if (state.text === ' - Select Location - ') {
-            return ' - Tipo de envío - '
-        }
-        return state.text.replace('Out of Stock', 'Agotado');
+  function locationFormatSelected(state) {
+    if (state.text === " - Select Location - ") {
+      return " - Tipo de envío - ";
     }
+    return state.text.replace("Out of Stock", "Agotado");
+  }
 
-    $('#select_location').select2({
-        minimumResultsForSearch: Infinity,
-        templateResult: locationFormatResult,
-        templateSelection: locationFormatSelected
-    });
+  // $('#select_location').select2({
+  //     minimumResultsForSearch: Infinity,
+  //     templateResult: locationFormatResult,
+  //     templateSelection: locationFormatSelected
+  // });
 });
