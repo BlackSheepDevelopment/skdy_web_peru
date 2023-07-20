c68cbbadf50a23c88ba3c32e60a5ea240dd6ff65
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-19 21:56:56 -0500

    :sparkles: Console log options

--- extra/woo-variations-select2.js
+++ extra/woo-variations-select2.js
@@ -86,7 +86,7 @@ jQuery(document).ready(function ($) {
     // Step 1: Remove options with class name attribute and data-lc-qty equal to 0
 
     const selectElement = $("#select_location");
-    var options = selectElement.find("option");
+    var options = selectElement.val();
 
     console.log(options);
     // Step 1: Remove options with class name attribute and data-lc-qty equal to 0
