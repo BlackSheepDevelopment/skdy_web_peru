c572f1a62eacca7fed87fc5366e2d7e507898f62
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 17:01:35 -0500

    :sparkles: Add changeshipment to doment ready

--- extra/woo-variations-select2.js
+++ extra/woo-variations-select2.js
@@ -97,39 +97,39 @@ jQuery(document).ready(function ($) {
     $("#select_location").val("-1");
     $("#select_location").trigger("change");
   }
-});
 
-function changeShipment() {
-  console.log("Testing inside function");
-  const selectElement = $("#select_location")[0];
-  const selectOptions = selectElement.options;
-  let desiredShipment = "0";
-  let savarData = selectOptions[1];
-  let primeData = selectOptions[2];
+  function changeShipment() {
+    console.log("Testing inside function");
+    const selectElement = $("#select_location")[0];
+    const selectOptions = selectElement.options;
+    let desiredShipment = "0";
+    let savarData = selectOptions[1];
+    let primeData = selectOptions[2];
 
-  console.log(savarData.className);
-  console.log(primeData.className);
-  console.log(savarData.dataset.lcQty);
-  console.log(primeData.dataset.lcQty);
+    console.log(savarData.className);
+    console.log(primeData.className);
+    console.log(savarData.dataset.lcQty);
+    console.log(primeData.dataset.lcQty);
 
-  if (
-    savarData.className == "wclimloc_savar24" &&
-    savarData.dataset.lcQty &&
-    parseInt(savarData.dataset.lcQty) > 0
-  ) {
-    desiredShipment = "0";
-  } else if (
-    primeData.className == "wclimloc_almacen" &&
-    primeData.dataset.lcQty &&
-    parseInt(primeData.dataset.lcQty) > 0
-  ) {
-    desiredShipment = "1";
-  } else {
-    desiredShipment = "0";
+    if (
+      savarData.className == "wclimloc_savar24" &&
+      savarData.dataset.lcQty &&
+      parseInt(savarData.dataset.lcQty) > 0
+    ) {
+      desiredShipment = "0";
+    } else if (
+      primeData.className == "wclimloc_almacen" &&
+      primeData.dataset.lcQty &&
+      parseInt(primeData.dataset.lcQty) > 0
+    ) {
+      desiredShipment = "1";
+    } else {
+      desiredShipment = "0";
+    }
+
+    console.log("desiredShipment: ");
+    console.log(desiredShipment);
+    $("#select_location").val(desiredShipment);
+    $("#select_location").trigger("change");
   }
-
-  console.log("desiredShipment: ");
-  console.log(desiredShipment);
-  $("#select_location").val(desiredShipment);
-  $("#select_location").trigger("change");
-}
+});
