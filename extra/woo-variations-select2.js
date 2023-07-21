9fcb4e185b7d52899003d8ca5a5f1fd2510badd9
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 17:24:44 -0500

    :sparkles: Reduce time of update

--- extra/woo-variations-select2.js
+++ extra/woo-variations-select2.js
@@ -93,7 +93,7 @@ jQuery(document).ready(function ($) {
     changeShipment();
   });
 
-  setTimeout(triggerShipping, 3000);
+  setTimeout(triggerShipping, 2000);
 
   function triggerShipping() {
     $("#select_location").val("-1");
