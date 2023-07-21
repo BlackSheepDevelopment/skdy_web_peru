779ae9de5f3f2678bbf24255c770a2a3c24901a3
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-21 16:00:06 -0500

    :sparkles: Update to on change to deug after, then sumit correct

--- src/entries/product.js
+++ src/entries/product.js
@@ -151,7 +151,7 @@ function changeShipment() {
   $("#select_location").trigger("change");
 }
 
-console.log("Testing outside function 7");
+console.log("Testing outside function 8");
 console.log($("#select_location"));
 
 $("#select_location").on("change", function (e) {
@@ -161,5 +161,5 @@ $("#select_location").on("change", function (e) {
   console.log($(".wcmlim_cart_valid_err").length);
   console.log("que pasaaa ");
   debugger;
-  $("variations_form").submit();
+  $(".variations_form").submit();
 });
