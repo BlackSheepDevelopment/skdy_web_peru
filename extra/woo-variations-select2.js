f25b658472563840e5c6d0f6006e51c7b9099d33
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-22 15:47:08 -0500

    :sparkles: Correctly console log

--- extra/woo-variations-select2.js
+++ extra/woo-variations-select2.js
@@ -84,12 +84,8 @@ jQuery(document).ready(function ($) {
     templateSelection: locationFormatSelected,
   });
 
-  console.log("Hello 11");
-  console.log($("#select_location").length);
-  console.log($("#select_location"));
-
   $("#select_location").on("change", function (e) {
-    console.log("an event was created");
+    console.log("Changing the shipment...");
     changeShipment();
   });
 
@@ -101,19 +97,17 @@ jQuery(document).ready(function ($) {
   }
 
   function changeShipment() {
-    console.log("Changing the shipment");
     if (!changedShipment) {
-      console.log("Testing inside function 14");
+      console.log("Working...");
       const selectElement = $("#select_location")[0];
       const selectOptions = selectElement.options;
       let desiredShipment = "0";
       let savarData = selectOptions[1];
       let primeData = selectOptions[2];
 
-      console.log(savarData.className);
-      console.log(primeData.className);
-      console.log(savarData.dataset.lcQty);
-      console.log(primeData.dataset.lcQty);
+      console.log(
+        `${savarData.className}: ${savarData.dataset.lcQty} | ${primeData.className}: ${primeData.dataset.lcQty}`
+      );
 
       if (
         savarData.className == "wclimloc_savar24" &&
@@ -131,8 +125,7 @@ jQuery(document).ready(function ($) {
         desiredShipment = "0";
       }
 
-      console.log("desiredShipment: ");
-      console.log(desiredShipment);
+      console.log(`Cambiar a: ${desiredShipment === "1" ? "Prime" : "Savar"} `);
       changedShipment = true;
       $("#select_location").val(desiredShipment);
       $("#select_location").trigger("change");
