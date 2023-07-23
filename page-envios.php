37e7e0ee3cefe8790946884f8aa33cc76f768341
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-23 17:19:55 -0500

    :sparkles: Add containers

--- page-envios.php
+++ page-envios.php
@@ -15,15 +15,15 @@ get_header(); ?>
                     <p>El tiempo de entrega es de 3 a 7 días hábiles.</p>
                 </div>
             </div>
-        </div>
-        <div class="envios-container__right">
-            <div class="envios-container__right__title">
-                Envío Regular
-            </div>
-            <div class="envios-container__right__content">
-                <p>Realizamos envíos a CABA y GBA a través de <strong>Moto Mensajería</strong>.</p>
-                <p>El costo de envío se calcula automáticamente al ingresar tu código postal en el carrito de compras.</p>
-                <p>El tiempo de entrega es de 24 a 48 hs hábiles.</p>
+            <div class="envios-container__right">
+                <div class="envios-container__right__title">
+                    Envío Regular
+                </div>
+                <div class="envios-container__right__content">
+                    <p>Realizamos envíos a CABA y GBA a través de <strong>Moto Mensajería</strong>.</p>
+                    <p>El costo de envío se calcula automáticamente al ingresar tu código postal en el carrito de compras.</p>
+                    <p>El tiempo de entrega es de 24 a 48 hs hábiles.</p>
+                </div>
             </div>
         </div>
     </div>
