366c74c65efa4fd3d993cd9895804f72628476ad
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-23 11:53:19 -0500

    :hammer: Append values

--- functions.php
+++ functions.php
@@ -700,7 +700,7 @@ function visit_location_script() { ?>
 
         function showProductNotice() {
             const _class = jQuery('#select_location option:selected').attr('class');
-			const text = 'Envío Rápido hasta agotar stock. Si no hay Envío Rápido cambia a Envío Regular en el checkout. <a href="https://skullcandy.com.pe/legales" target="_blank">REVISA AQUÍ ANTES DE COMPRAR</a>.'
+			const text = '<p class="more-notice__check" >Envío Rápido hasta agotar stock. Si no hay Envío Rápido cambia a Envío Regular en el checkout. <a class="more-notice__check" href="https://skullcandy.com.pe/legales" target="_blank">REVISA AQUÍ ANTES DE COMPRAR</a> </p>'
 			jQuery('.product-notice').html(text);
         }
 
