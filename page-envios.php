6e269503fd5fa47a11091f3c01ab5b5897cd6942
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-23 17:17:58 -0500

    :sparkles: Correct containers

--- page-envios.php
+++ page-envios.php
@@ -7,17 +7,18 @@ get_header(); ?>
         <div class="envios-container">
             <div class="envios-container__left">
                 <div class="envios-container__left__title">
-                    <h2>Envío Rápido</h2>
+                    Envío Rápido
                 </div>
-            <div class="envios-container__left__content">
-                <p>Realizamos envíos a todo el país a través de <strong>Correo Argentino</strong> y <strong>Andreani</strong>.</p>
-                <p>El costo de envío se calcula automáticamente al ingresar tu código postal en el carrito de compras.</p>
-                <p>El tiempo de entrega es de 3 a 7 días hábiles.</p>
+                <div class="envios-container__left__content">
+                    <p>Realizamos envíos a todo el país a través de <strong>Correo Argentino</strong> y <strong>Andreani</strong>.</p>
+                    <p>El costo de envío se calcula automáticamente al ingresar tu código postal en el carrito de compras.</p>
+                    <p>El tiempo de entrega es de 3 a 7 días hábiles.</p>
+                </div>
             </div>
         </div>
         <div class="envios-container__right">
             <div class="envios-container__right__title">
-                <h2>Envío Regular</h2>
+                Envío Regular
             </div>
             <div class="envios-container__right__content">
                 <p>Realizamos envíos a CABA y GBA a través de <strong>Moto Mensajería</strong>.</p>
