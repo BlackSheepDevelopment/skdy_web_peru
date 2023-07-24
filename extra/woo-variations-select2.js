0afc8e9e8ecff885a00ad70367c39c32ba284656
elmerescandon <elmer.escandon@utec.edu.pe>
2023-07-24 17:28:47 -0500

    :sparkles: Update remove container

--- extra/woo-variations-select2.js
+++ extra/woo-variations-select2.js
@@ -128,7 +128,7 @@ jQuery(document).ready(function ($) {
         desiredShipment = "0";
         if ($(".no-stock__container").length > 0) return;
         $(".Wcmlim_container").append(
-          "<div class='no-stock__container'>El producto se encuentra agotado.</div>"
+          `<div class="no-stock__container">El producto se encuentra agotado.</div>`
         );
         event.preventDefault();
         return;
