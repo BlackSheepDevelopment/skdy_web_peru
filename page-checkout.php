dfe7da7373fbae101a3b783e71d0c3b6448d326a
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-10 17:44:56 -0500

    :sparkles: Update coupon

--- page-checkout.php
+++ page-checkout.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 			<div class="coupon-container">
 				<div class="coupon-container__title">¿Te gustaría recibir un 15% de descuento en esta compra?</div>
-				<a class="coupon-container__link"  target=”_blank” href="https://forms.office.com/r/sgWrV96UJw">¡Haz click aquí y responde esta encuesta!</a>
+				<a class="coupon-container__link"  target=”_blank” href="https://forms.office.com/r/sgWrV96UJw">¡Responde la encuesta!</a>
 			</div>
 
 			<?php
