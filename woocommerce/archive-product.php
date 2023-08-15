e1084e68c79fe19a7d2a662b87a27e9b42ff836f
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-15 17:09:01 -0500

    :lipstick: Update buttons comment

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -72,11 +72,11 @@ do_action( 'woocommerce_before_main_content' );
 
 <div class="filter">
 	<div>
-		<button class="filter__section__button">Ver Todo</button>
-		<button class="filter__section__button">True Wireless</button>
-		<button class="filter__section__button">Headphones</button>
-		<button class="filter__section__button">Bluetooth</button>
-		<button class="filter__section__button">Cableados</button>
+		<button>Ver Todo</button>
+		<button>True Wireless</button>
+		<button>Headphones</button>
+		<button>Bluetooth</button>
+		<button>Cableados</button>
 	</div>
 
 	<div class="filter__section">
