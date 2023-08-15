c9cb41fb300c380cafb6ae9db6b108e9f56ad1e3
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-15 13:32:06 -0500

    :sparkles: Show all products

--- src/entries/shop.js
+++ src/entries/shop.js
@@ -10,12 +10,10 @@ $("#sidebar-shop .mobile-collapse").click(function (event) {
 $(".filter-category").on("change", function (event) {
     let cat_val = event.target.value;
     let products = $(".product");
-    // Filter products that also have the class "product-earbuds"
-    let filteredProducts = $(`.product_cat-${cat_val}`);
 
-    // Hide all products first
     products.hide();
-
-    // Show only the filtered products
-    filteredProducts.show();
+    if (cat_val !== "") {
+        let filteredProducts = $(`.product_cat-${cat_val}`);
+        filteredProducts.show();
+    }
 });
