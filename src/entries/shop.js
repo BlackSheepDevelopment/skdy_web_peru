16a57bcd869ffa0e866ffbc52f6cb30790b4e020
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-15 13:08:30 -0500

    :sparkles: Update the class filtering products

--- src/entries/shop.js
+++ src/entries/shop.js
@@ -11,4 +11,7 @@ $("filter-category").on("change", function (event) {
     let cat_val = event.target.value;
     let products = $(".product");
     console.log(products);
+
+    let filteredProducts = products.filter(cat_val);
+    console.log(filteredProducts);
 });
