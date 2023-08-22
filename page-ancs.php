3de3fc391450909f1d150132003d83e5bdeeac5c
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-22 18:56:40 -0500

    :lipstick: Correct styling of the anc page

--- page-ancs.php
+++ page-ancs.php
@@ -3,10 +3,10 @@ get_header(); ?>
 
 <div class="main_anc">
     <div class="content_anc">	
-        <img src="<?php echo get_template_directory_uri(); ?>/src/assets/ANC_SIDEBANNER.png" alt="ancs">
-        <div>
+        <img class="image_anc" src="<?php echo get_template_directory_uri(); ?>/src/assets/ANC_SIDEBANNER.png" alt="ancs">
+        <div class="info_anc">
             <h1 class="header_anc">¿Qué es la cancelación de ruido o ANC?</h1>
-            <p class="text-anc ">La cancelación de ruido activa (ANC) es una tecnología que permite eliminar el ruido no deseado de un entorno a través de la generación de ondas sonoras de fase opuesta. Esta tecnología se ha utilizado durante años en los auriculares de gama alta, pero ahora se está volviendo más común en los auriculares inalámbricos de gama media.</p>
+            <p class="text_anc ">La cancelación de ruido activa (ANC) es una tecnología que permite eliminar el ruido no deseado de un entorno a través de la generación de ondas sonoras de fase opuesta. Esta tecnología se ha utilizado durante años en los auriculares de gama alta, pero ahora se está volviendo más común en los auriculares inalámbricos de gama media.</p>
         </div>
         
     </div>
