2c969c4d5ac48f8514423e3d14e24ba79c3076d9
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-23 14:52:47 -0500

    :bug: Add slash to assets imge

--- templates/home.php
+++ templates/home.php
@@ -15,9 +15,9 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
     <div class="main-container`">
 		<?php if ( have_rows( 'header' ) ): ?>
             <div id="home-grid">
-                <source srcset="<?php echo get_template_directory_uri() ?>src/assets/MAIN_BANNER_MOV.png"
+                <source srcset="<?php echo get_template_directory_uri() ?>/src/assets/MAIN_BANNER_MOV.png"
                         media="(max-width: 550px)"/>
-                <img src="<?php echo get_template_directory_uri() ?>src/assets/MAIN_BANNER_WEB_DESK.png" alt="Home Grid v2"
+                <img src="<?php echo get_template_directory_uri() ?>/src/assets/MAIN_BANNER_WEB_DESK.png" alt="Home Grid v2"
                      class="home-grid">
             </div>
 		<?php endif; ?>
