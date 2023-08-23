4468e4e51658704d838ef7ac9aad32b67db95503
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-22 21:36:53 -0500

    :sparkles: Add new list items

--- page-ancs.php
+++ page-ancs.php
@@ -6,7 +6,13 @@ get_header(); ?>
         <img class="image_anc" src="<?php echo get_template_directory_uri(); ?>/src/assets/ANC_SIDEBANNER.png" alt="ancs">
         <div class="info_anc">
             <h1 class="header_anc">¿Qué es la cancelación de ruido o ANC?</h1>
-            <p class="text_anc ">La cancelación de ruido activa (ANC) es una tecnología que permite eliminar el ruido no deseado de un entorno a través de la generación de ondas sonoras de fase opuesta. Esta tecnología se ha utilizado durante años en los auriculares de gama alta, pero ahora se está volviendo más común en los auriculares inalámbricos de gama media.</p>
+            <h2>Experimenta la Calma Absoluta: Descubre los Beneficios de la Cancelación Activa de Ruido (ANC) en tus Skullcandy.</h2>
+            <ul>
+                <li>Sumérgete en tu mundo: Aísla el ruido externo y sumérgete en tu música con una claridad incomparable.</li>
+                <li>Concentración sin esfuerzo: Elimina distracciones para un enfoque total en el trabajo, estudio o relajación.</li>
+                <li>Menos volumen, más seguridad: Disfruta de tus pistas a volúmenes más bajos sin comprometer la calidad sonora.</li>
+                <li>Viajes tranquilos: Reduce drásticamente el ruido del avión, trenes y coches, creando viajes más relajantes.</li>
+            </ul>
         </div>
         
     </div>
