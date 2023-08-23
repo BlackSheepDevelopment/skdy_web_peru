d9df546e5e8cae97ab72775cbc0c7bf5eda2f7dc
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-22 21:41:25 -0500

    :lipstick: Add new values to anc values

--- page-ancs.php
+++ page-ancs.php
@@ -7,11 +7,11 @@ get_header(); ?>
         <div class="info_anc">
             <h1 class="header_anc">¿Qué es la cancelación de ruido o ANC?</h1>
             <h2>Experimenta la Calma Absoluta: Descubre los Beneficios de la Cancelación Activa de Ruido (ANC) en tus Skullcandy.</h2>
-            <ul>
-                <li>Sumérgete en tu mundo: Aísla el ruido externo y sumérgete en tu música con una claridad incomparable.</li>
-                <li>Concentración sin esfuerzo: Elimina distracciones para un enfoque total en el trabajo, estudio o relajación.</li>
-                <li>Menos volumen, más seguridad: Disfruta de tus pistas a volúmenes más bajos sin comprometer la calidad sonora.</li>
-                <li>Viajes tranquilos: Reduce drásticamente el ruido del avión, trenes y coches, creando viajes más relajantes.</li>
+            <ul class="list_anc">
+                <li class="list_item_anc">Sumérgete en tu mundo: Aísla el ruido externo y sumérgete en tu música con una claridad incomparable.</li>
+                <li class="list_item_anc">Concentración sin esfuerzo: Elimina distracciones para un enfoque total en el trabajo, estudio o relajación.</li>
+                <li class="list_item_anc">Menos volumen, más seguridad: Disfruta de tus pistas a volúmenes más bajos sin comprometer la calidad sonora.</li>
+                <li class="list_item_anc">Viajes tranquilos: Reduce drásticamente el ruido del avión, trenes y coches, creando viajes más relajantes.</li>
             </ul>
         </div>
         
