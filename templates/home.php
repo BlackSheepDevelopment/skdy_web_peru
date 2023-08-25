fc76279c6c9c064bacb8f678145a15e013894a32
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-25 15:12:17 -0500

    :fire: Eliminate all home page

--- templates/home.php
+++ templates/home.php
@@ -13,7 +13,6 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
     </div>
 <?php endif; ?>
     <div class="main-container`">
-		<?php if ( have_rows( 'header' ) ): ?>
             <div id="home-grid">
                 <picture>
                     <source srcset="<?php echo get_template_directory_uri() ?>/src/assets/MAIN_BANNER_WEB_DESK.png"
@@ -22,43 +21,5 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                         class="home-grid">
                 </picture>
             </div>
-		<?php endif; ?>
-		<?php if ( have_rows( 'banners' ) ):
-			while ( have_rows( 'banners' ) ): the_row(); ?>
-				<?php if ( get_sub_field( 'image_full' ) ): ?>
-                    <div class="full-width">
-						<?php $full = get_sub_field( 'image_full' ); ?>
-                        <a href="<?php echo $full['link']; ?>">
-                            <picture>
-								<?php $img = $full['image']; ?>
-                                <source srcset="<?php echo $img['desktop']['url'] ?>" media="(min-width: 551px)"/>
-                                <img src="<?php echo $img['mobile']['url'] ?>"
-                                     alt="<?php echo $img['mobile']['alt'] ?>">
-                            </picture>
-                        </a>
-                    </div>
-				<?php endif; ?>
-			<?php endwhile;
-		endif; ?>
-		<?php if ( have_rows( 'gallery' ) ): ?>
-            <div id="gallery">
-				<?php while ( have_rows( 'gallery' ) ): the_row(); ?>
-                    <a href="<?php the_sub_field( 'link' ); ?>" class="media">
-                        <picture>
-							<?php $img = get_sub_field( 'image' ); ?>
-                            <source srcset="<?php echo $img['desktop']['url'] ?>" media="(min-width: 551px)"/>
-                            <img src="<?php echo $img['mobile']['url'] ?>"
-                                 alt="<?php echo $img['mobile']['alt'] ?>">
-                        </picture>
-						<?php $hover = get_sub_field( 'hover' );
-						if ( $hover ): ?>
-                            <img src="<?php echo $hover['url'] ?>" alt="<?php echo $hover['alt'] ?>" class="hover">
-						<?php endif; ?>
-                    </a>
-				<?php endwhile; ?>
-            </div>
-		<?php endif; ?>
-
-
     </div>
 <?php get_footer();
