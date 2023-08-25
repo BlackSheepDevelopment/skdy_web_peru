60da4ec9e5d5492046411220e798ccc1e3e35ec3
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-25 16:33:53 -0500

    :lipstick: Correct home CSS

--- templates/home.php
+++ templates/home.php
@@ -27,9 +27,8 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                     <p class="home-title">SINTONIZA</p>
                     <p class="home-title">CADA MOMENTO</p>
                     <p class="home-title home-title--bold">CON LA FUNCIÓN PERFECTA</p>
-                    <img src="<?php echo get_template_directory_uri() ?>/src/assets/FEEL_SKULLCANDY_WHITE.png"/>
+                    <img class="home-title__image" src="<?php echo get_template_directory_uri() ?>/src/assets/FEEL_SKULLCANDY_WHITE.png"/>
                 </div>
-                  
             </div>
     </div>
 <?php get_footer();
