f357582f3d1e15b318d70592ac078b88acca04d6
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-28 22:48:23 -0500

    :bug: Add text to button

--- templates/home.php
+++ templates/home.php
@@ -106,7 +106,7 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                     </picture>
                     <div class="enterate__container__info">
                         <div class="enterate__container__item__title">Skullcandy ANC</div>
-                        <div class="enterate__container__item__subtitle">Conoce nuestros productos con cancelación de ruido.</div>
+                        <div class="enterate__container__item__subtitle">Conoce nuestros productos con cancelación de ruido para todo momento.</div>
                         <a class="enterate__container__item__button" href="https://skullcandy.com.pe/ancs/">Entérate más</a>
                     </div>
                 </div>
