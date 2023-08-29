27ed64b9b3565058b754c5b6d425b347f92a8da9
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-08-28 23:22:42 -0500

    :sparkles: Add new home page

--- templates/home.php
+++ templates/home.php
@@ -87,13 +87,13 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
         <div class="categories">
             <div  class="top-products__title">CATEGORÍAS.</div>
             <div class="categories-additionals">
-                <button class="categories-additionals__button categories-additionals__button--ofertas">OFERTAS</button>
-                <button class="categories-additionals__button categories-additionals__button--packs">PACKS</button>
+                <a href="https://skullcandy.com.pe/shop/?cat=ofertas" class="categories-additionals__button categories-additionals__button--ofertas">OFERTAS</a>
+                <a href="https://skullcandy.com.pe/shop/?cat=packs" class="categories-additionals__button categories-additionals__button--packs">PACKS</a>
             </div>
             <div class="categories-generals">
-                <button class="categories-generals__button categories-generals__button--headphones">HEADPHONES</button>
-                <button class="categories-generals__button categories-generals__button--bluetooth">BLUETOOTH</button>
-                <button class="categories-generals__button categories-generals__button--tw">TRUE WIRELESS</button>
+                <a href="https://skullcandy.com.pe/shop/?cat=headphones" class="categories-generals__button categories-generals__button--headphones">HEADPHONES</a>
+                <a href="https://skullcandy.com.pe/shop/?cat=bluetooth" class="categories-generals__button categories-generals__button--bluetooth">BLUETOOTH</a>
+                <a href="https://skullcandy.com.pe/shop/?cat=true-wireless" class="categories-generals__button categories-generals__button--tw">TRUE WIRELESS</a>
             </div>
         </div>
         <div class="enterate">
