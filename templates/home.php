de7b4c9bf2ba0538363ec56d590cb6fe58da4009
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-13 17:55:15 -0500

    :sparkles: Add space to offer

--- templates/home.php
+++ templates/home.php
@@ -104,7 +104,7 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                             <h2 class="product-title"><?php echo $product->get_name(); ?></h2>
                             <p class="product-price"><?php echo $product->get_price_html(); ?></p>
                         </div>
-                        <p class="product-off">40%</p>
+                        <p class="product-off">40% </p>
                     </a>
                 </div>
                 <div>
