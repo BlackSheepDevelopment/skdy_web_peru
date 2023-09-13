02aa64c3c781b8ee29526f68d17526dcbbfcd856
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-13 14:18:28 -0500

    :sparkles: Change naming and figures

--- templates/home.php
+++ templates/home.php
@@ -42,7 +42,7 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                 <div class="shop-features__container">
                     <svg width="30" height="30" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16"> <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/> </svg>
                     <div class="shop-features__container__info">
-                        <p class="shop-features__container__info__title">Envío gratis a nivel nacional sin mínimo de compra</p>
+                        <p class="shop-features__container__info__title">Envíamos el producto a cualquier parte del Perú</p>
                     </div>
 
                 </div>
@@ -152,12 +152,12 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
 
                 <div class="enterate__container__item">
                     <picture class="enterate__container__item__picture">
-                        <img src="<?php echo get_template_directory_uri() ?>/src/assets/ENVIOS_GRATIS_CROP.png" alt="skullcandy_enterate_2"
+                        <img src="<?php echo get_template_directory_uri() ?>/src/assets/BANNER_OFERTAS_CROPPED.png" alt="skullcandy_enterate_2"
                             class="enterate__container__item__picture__image">
                     </picture>
                     <div class="enterate__container__info">
-                        <div class="enterate__container__item__title">¡Envios gratis!</div>
-                        <div class="enterate__container__item__subtitle">Compra tus audífonos o packs y te lo envíamos gratis, sin monto mínimo.</div>
+                        <div class="enterate__container__item__title">¡Envío a toda parte del Perú!</div>
+                        <div class="enterate__container__item__subtitle">Compra tus audífonos o packs y te lo envíamos en donde sea que estés.</div>
                         <a class="enterate__container__item__button" href="<?php echo get_permalink( wc_get_page_id( 'shop' ) ); ?>">Entérate más</a>
                     </div>
                 </div>
