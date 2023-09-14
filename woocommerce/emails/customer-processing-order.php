38163361e19c8ec6c4f09a4e15dda89ddd449755
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-14 17:16:02 -0500

    :sparkles: New modifications

--- woocommerce/emails/customer-processing-order.php
+++ woocommerce/emails/customer-processing-order.php
@@ -30,21 +30,21 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 <p> Hemos recibido tu pedido #<strong><?php printf( esc_html( $order->get_order_number() ) ); ?></strong></p>
 
 <div style="padding-bottom:10px">
-	<h2 style="padding:8px">Si tu compra fue con:</h2>
-	<div style="padding:8px; color:black">
+	<h2 style="padding:8px 0;">Si tu compra fue con:</h2>
+	<div style="padding:8px 0;; color:black">
 		<h3 style="font-size:0.9rem; color:black; margin:0 0 6px">Envío Rápido</h3>
-		<p style="margin:0 0 15px; padding-left:5px">Lima Metropolitana y Callao:Se entregará entre 24 a 72 horas.</p>
-		<p style="margin:0 0 15px; padding-left:5px">Otras regiones: Se entregará entre 4 a 15 días hábiles según tu <a style="color:black" href="https://skullcandy.com.pe/wp-content/uploads/2022/01/LEAD-TIME-A-NIVEL-NACIONAL.pdf">region</a></p>
+		<p style="margin:0 0 15px;">Lima Metropolitana y Callao: Se entregará entre 24 a 72 horas.</p>
+		<p style="margin:0 0 15px;">Otras regiones: Se entregará entre 4 a 15 días hábiles según tu <a style="color:black" href="https://skullcandy.com.pe/wp-content/uploads/2022/01/LEAD-TIME-A-NIVEL-NACIONAL.pdf">región.</a></p>
 		<a style="color:white; background-color:black; padding:8px 13px; text-decoration:none; margin:0 0 16px" href="https://www.savarexpress.com.pe/rastrea-un-envio/?cod=SKDY<?php echo $order -> get_order_number()?>">Ver pedido</a>
 	</div>
 	<hr/>
-	<div style="padding:8px; color:black;">
+	<div style="padding:8px 0; color:black;">
 		<h3 style="font-size:0.9rem; color:black; margin: 0 0 6px">Envío Regular</h3>
-		<p style="margin:0 0 15px;">Lima Metropolitana y Callao:Se entregará entre 48 a 72 horas</p>
-		<p style="margin:0 0 15px;">Otras regiones:Se entregará entre 4 a 15 días hábiles (<strong><a href="https://wa.link/xfavry">Consúltanos por tu link de seguimiento</a></strong>) </p>
+		<p style="margin:0 0 15px;">Lima Metropolitana y Callao: Se entregará entre 48 a 72 horas.</p>
+		<p style="margin:0 0 15px;">Otras regiones: Se entregará entre 4 a 15 días hábiles (<strong><a href="https://wa.link/xfavry">Consúltanos por tu link de seguimiento</a></strong>).</p>
 		<a style="color:white; background-color:black; padding:8px 13px; text-decoration:none;" href="https://www.prime-express.pe/trackstatus/tracking/px-det-op.php?npedido=<?php echo $order -> get_order_number()?>&dni=<?php echo $order->get_meta('billing_id')?>">Ver pedido (SOLO LIMA Y CALLAO)</a>
 		<p style="margin:15px 0 2px; font-size:0.8rem; color:gray; font-style:italic;">*Para compras realizadas entre Lunes y Jueves: La página de seguimiento se activará a partir de las 10AM del siguiente día.</p>
-		<p style="margin:0 0 2px; font-size:0.8rem; color:gray; font-style:italic;">**Para compras realizadas entre Viernes y Domingo: La página de seguimiento se activará a partir del Lunes siguiente a las 10AM </p>
+		<p style="margin:0 0 2px; font-size:0.8rem; color:gray; font-style:italic;">**Para compras realizadas entre Viernes y Domingo: La página de seguimiento se activará a partir del Lunes siguiente a las 10AM.</p>
 	</div>
 	<hr/>
 	<p><a style="color:black; margin:0 0 10px" href="https://skullcandy.com.pe/envios/">Obtén más información acerca de tu pedido aquí</a></p>
