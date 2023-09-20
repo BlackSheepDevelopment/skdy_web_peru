9e1f3e23c2e8d9d962d8b2feac6c7b843de32ba5
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-09-20 12:26:08 -0500

    :sparkles: Implement forms

--- page-mayoristas.php
+++ page-mayoristas.php
@@ -2,7 +2,14 @@
 get_header(); ?>
 
 <div class="main">
-    <h1>Inicio de página para mayoristas</h1>
+    <div>
+        <h1>MAYORISTAS</h1>
+        <p>Gracias por tu interés en comprar Skullcandy para tu negocio.</p>
+        <p>Los productos mostrados en nuestro catálogo web son los disponibles para la compra al por mayor</p>
+        <?php echo do_shortcode( '[contact-form-7 id="161606" title="formulario-mayoristas"]' ); ?>
+
+
+    </div>
 </div>
 
 
