957c0b414712529b20bcf4ce9f01f933b849967f
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-05 22:07:07 -0500

    :sparkles: Update home

--- templates/home.php
+++ templates/home.php
@@ -62,6 +62,7 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                 <div class="top-products__title">MÁS VISTOS.</div>
                 <div class="top-products-container">
                    <?php while(have_rows('carrusel')): the_row(); ?>
+                   
                         <?php
                             $product_id = get_sub_field('id'); 
                             $product = wc_get_product($product_id);
