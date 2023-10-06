fb5959c7b784499358dcf25830a999f7f12de16d
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-05 23:33:09 -0500

    :sparkles: Update the container

--- templates/home.php
+++ templates/home.php
@@ -37,6 +37,7 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                     <video class="home-grid__picture" src="<?php echo get_template_directory_uri() ?>/src/assets/PORTADA_WEB_NEWGEN.mp4" autoplay muted loop playsinline> </video>
                 </picture>
                 <div>
+                    
                     <picture class="home-grid__video">
                         <video class="home-grid__picture" src="<?php echo get_template_directory_uri() ?>/src/assets/PORTADA_WEB_SKDY.mp4" autoplay muted loop playsinline> </video>
                     </picture>
