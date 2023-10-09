1e43747685f648199256d21d9ab95e6f098d1725
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-09 12:49:25 -0500

    :sparkles: Update smokin buds title

--- templates/home.php
+++ templates/home.php
@@ -47,7 +47,7 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                         <video class="home-grid__picture" src="<?php echo get_template_directory_uri() ?>/src/assets/PORTADA_SMOKIN.mp4" autoplay muted loop playsinline> </video>
                     </picture>
                     <div class="home-info">
-                            <p class="home-title home-title--bold">Smokin'Buds</p>
+                            <p class="home-title home-title--bold">SMOKIN' BUDS</p>
                             <p class="home-title home-title--bold">ESTILO. POTENCIA. CALIDAD.</p>
                             <div>
                                 <a class="home-title__button" href="https://skullcandy.com.pe/producto/smokin-buds-true-wireless-eardbuds/">¡COMPRALOS YA!</a>
