72e30a5efade5284d8fbbf4b4d7ca57b50938dc6
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-17 00:32:08 -0500

    :lipstick: Add image of mayoristas

--- page-retailers.php
+++ page-retailers.php
@@ -27,7 +27,7 @@ get_header(); ?>
                         <div class="product-container">
                             <?php $variations = $product->get_available_variations(); ?> 
                             <a href="<?php echo $product -> get_permalink() ?>" class="product">
-                                <img class="product-image" src="<?php echo $product->get_image()?>"/>
+                                <img class="product-image" src="<?php echo $product->get_image()?>
                                 <p class="product-name"> <?php echo $product->get_name() ?></p>
                                 <p clas="product-price"> <?php echo $product->get_price_html() ?></p>
                             </a>
