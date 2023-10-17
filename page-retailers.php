4206c1f8197291a908f62efc91f5a78e25c14646
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-16 23:47:28 -0500

    :sparkles: Update page retailers classes

--- page-retailers.php
+++ page-retailers.php
@@ -20,33 +20,38 @@ get_header(); ?>
             $products = wc_get_products($args);
 
             // Check if there are products in the array
-            if (!empty($products)) {
-                foreach ($products as $product) {
-                    $variations = $product->get_available_variations();
-                    ?> 
-                    <a href="<?php echo $product -> get_permalink() ?>">
-                        <p> <?php echo $product->get_name() ?></p> 
-                    </a>
-
-                    <div class="list-swatches">
-                        <?php foreach ( $variations as $variation ) {
-                            $name = $variation['attributes']['attribute_color'];
-                            ?>
-                                <a class="swatch-link"
-                                href="<?php echo $product->get_permalink(); ?>?attribute_color=<?php echo $name ?>">
-                                    <img src="<?php echo $variation['image']['url'] ?>"
-                                        alt="<?php echo $name; ?>"
-                                        title="<?php echo $name; ?>">
-                                    <p class="swatch-name"><?php echo $name; ?></p>
-                                </a>
-                        <?php } 
+            if (!empty($products)) { ?>
+                <div class="product-list"> <?php
+                    foreach ($products as $product) {
                         ?>
-                    </div>
+                        <div class="product">
+                            <?php $variations = $product->get_available_variations(); ?> 
+                            <a href="<?php echo $product -> get_permalink() ?>">
+                                <p> <?php echo $product->get_name() ?></p>
+                                <img src="<?php echo $product->get_image() ?>" alt="<?php echo $product->get_name() ?>">
+                                <p> <?php echo $product->get_price_html() ?></p>
+                            </a>
 
-                    <?php
-                }
+                            <div class="list-swatches">
+                                <?php foreach ( $variations as $variation ) {
+                                    $name = $variation['attributes']['attribute_color'];
+                                    ?>
+                                        <a class="swatch-link"
+                                        href="<?php echo $product->get_permalink(); ?>?attribute_color=<?php echo $name ?>">
+                                            <img src="<?php echo $variation['image']['url'] ?>"
+                                                alt="<?php echo $name; ?>"
+                                                title="<?php echo $name; ?>">
+                                            <p class="swatch-name"><?php echo $name; ?></p>
+                                        </a>
+                                <?php } ?>
+                            </div>
+                            
+                        </div>
+                    <?php } ?>
+                </div>
+                <?php
             } else {
-                echo '<p>No products found.</p>';
+                echo '<p class="no-product">No products found.</p>';
             }
         ?>
 
