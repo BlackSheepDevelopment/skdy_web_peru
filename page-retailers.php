5f51b42120f004741db01b80dfc1c855968a98a7
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-16 21:06:16 -0500

    :sparkles: Update data for all the products

--- page-retailers.php
+++ page-retailers.php
@@ -2,8 +2,21 @@
 get_header(); ?>
 
 <div class="main">
-    <div>NO SE QUE HACER SOLO QUIERO QUE SE ACTUALICE</div>
-    HELLO
+    <div class="header">Tienda para Mayoristas</div>
+    <p class="subtitle">Gracias por tu interés en comprar Skullcandy para tu tienda. Nosotros te ofrecemos:</p>
+    <ul class="mayorista-features">
+        <li class="mayorista-features__item">Nuestro catálogo mayorista está disponible desde compras mínimas de S/.750</li>
+        <li class="mayorista-features__item">Despacho de los productos a tu almacén</li>
+        <li class="mayorista-features__item">Pago en línea mediante tarjeta de crédito o débito. Si deseas realizar algún depósito bancario, contáctate con contacto@blacksheep.com.pe</li>
+    </ul>
+
+
+    <div class="mayorista-store">
+        <div clas="mayorista-store__title">
+            PRODUCTOS
+        </div>
+        <?php print_r($woocommerce->get('products')); ?>
+    </div>
 </div>
 
 
