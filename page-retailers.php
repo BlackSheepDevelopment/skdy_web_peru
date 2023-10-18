6390866f69eec4588e257501b58a385312fe2c1d
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-18 18:59:19 -0500

    :sparkles: Add to cart

--- page-retailers.php
+++ page-retailers.php
@@ -74,7 +74,18 @@ get_header(); ?>
                                 </div>
 
                             </div>
-                            <?php echo do_shortcode('[wcmlim_locations_switch]'); ?>
+                            <?php
+                                echo apply_filters( 'woocommerce_loop_add_to_cart_link',
+                                    sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" class="button %s product_type_%s">%s</a>',
+                                        esc_url( $product->add_to_cart_url() ),
+                                        esc_attr( $product->get_id() ),
+                                        esc_attr( $product->get_sku() ),
+                                        $product->is_purchasable() ? 'add_to_cart_button' : '',
+                                        esc_attr( $product->get_type() ),
+                                        esc_html( $product->add_to_cart_text() )
+                                    ),
+                                $product );
+                            ?>
                         </div>
                     <?php } ?>
                 </div>
