487806e59e16ea3f3f81c757dadab83f8aef5bc8
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-18 16:58:01 -0500

    :sparkles: Set the limit to 3

--- page-retailers.php
+++ page-retailers.php
@@ -30,7 +30,7 @@ get_header(); ?>
 
     <div class="mayorista-store">
         <?php 
-            $args = array('visibility' => 'catalog', 'category' => array( 'retailer' ), 'limit' => 10);
+            $args = array('visibility' => 'catalog', 'category' => array( 'retailer' ), 'limit' => 3);
             $products = wc_get_products($args);
 
             // Check if there are products in the array
