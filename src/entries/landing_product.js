d6d043f7fcee55e0377b4d5b1fb85a84cbe94904
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-19 08:01:46 -0500

    :sparkles: Add conditions on the page

--- src/entries/landing_product.js
+++ src/entries/landing_product.js
@@ -1,1 +1,12 @@
-import '../scss/landing_product.scss';
+import "../scss/landing_product.scss";
+import $ from "jquery";
+
+$(document).ready(function () {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const type = urlParams.get("type");
+
+    if (type === "mayorist") {
+        $(".mayorista_header").css("display", "block");
+    }
+});
