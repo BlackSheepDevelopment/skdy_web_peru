e977587d941d3c4bffedda78f629d1f4f0c9d4cc
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-10-25 16:29:34 -0500

    :sparkles: Update values un web page

--- templates/home.php
+++ templates/home.php
@@ -21,12 +21,16 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                 </svg>
             </a>
 
-            <!-- <a class="home-grid__social__item" href="https://www.instagram.com/skullcandyperu/?hl=es-la" target="_blank">
-                <img src="<?php #echo get_template_directory_uri() ?>/src/assets/IG_SVG.svg"/>
+            <a class="home-grid__social__item" href="https://www.instagram.com/skullcandyperu/?hl=es-la" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" class="icon-social__fb">
+                    <path d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"></path>
+                </svg>
             </a>
-            <a class="home-grid__social__item" href="https://www.tiktok.com/@skullcandyperu">
-                <img src="<?php #echo get_template_directory_uri() ?>/src/assets/TT_SVG.svg"/>
-            </a> -->
+            <a class="home-grid__social__item" href="https://www.tiktok.com/@skullcandyperu" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 24 24" class="icon-social__fb">
+                    <path d="M 6 3 C 4.3550302 3 3 4.3550302 3 6 L 3 18 C 3 19.64497 4.3550302 21 6 21 L 18 21 C 19.64497 21 21 19.64497 21 18 L 21 6 C 21 4.3550302 19.64497 3 18 3 L 6 3 z M 12 7 L 14 7 C 14 8.005 15.471 9 16 9 L 16 11 C 15.395 11 14.668 10.734156 14 10.285156 L 14 14 C 14 15.654 12.654 17 11 17 C 9.346 17 8 15.654 8 14 C 8 12.346 9.346 11 11 11 L 11 13 C 10.448 13 10 13.449 10 14 C 10 14.551 10.448 15 11 15 C 11.552 15 12 14.551 12 14 L 12 7 z"></path>
+                </svg>
+            </a>
             <!-- <div class="home-grid__social__item home-grid__social__item--name">NEWSLETTER</div> -->
         </div>
 
