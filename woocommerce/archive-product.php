ec9688072f7962410daf345562df9c2104f1df4c
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-14 16:56:03 -0500

    :lipstick;: Updae values

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -35,11 +35,6 @@ do_action( 'woocommerce_before_main_content' );
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 		<!-- <h1 class="woocommerce-products-header__title page-title page-title-shop"><?php #woocommerce_page_title(); ?></h1> -->
 	<?php endif; ?>
-
-
-
-
-
 	<?php
 	/**
 	 * Hook: woocommerce_archive_description.
