daec28c15aa9cee16709771e9ddd8de3d1519bca
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-14 15:19:25 -0500

    Remove unnecessary code in review-order.php

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -40,7 +40,7 @@ defined( 'ABSPATH' ) || exit;
 						<?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 					</td>
 					<td class="product-total">
-						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); ?>
 						<!-- Get discount for this product -->
 						<?php 
 							global $product_discounts;
