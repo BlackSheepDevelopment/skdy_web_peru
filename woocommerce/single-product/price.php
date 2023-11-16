35a2ac8537e2a03d3c0e1de6cf9f1e7ca1273a18
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-16 16:19:59 -0500

    :sparkles: Upate price testing

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -22,4 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product;
 
 ?>
-<p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>"><?php echo $product->get_price_html(); ?></p>
+<p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>">
+	<?php echo $product->get_price_html(); ?>
+	<p>HOLA COMO ESTA </p>
+</p>
