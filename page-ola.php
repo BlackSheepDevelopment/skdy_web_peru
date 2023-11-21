e363ff63f7f5d0d369be4f3774993307fa5da5b2
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-21 11:15:41 -0500

    Update banner image in page-ola.php

--- page-ola.php
+++ page-ola.php
@@ -2,7 +2,7 @@
 get_header(); ?>
 
 <div>
-    HOLA ME GUSTAN LAS OLAS DEL MAR
+    <img src="<?php echo get_stylesheet_directory_uri() . '/uploads/OLA_BANNER_MAIN.webp' ?>" alt="skullcandy-ola"/>
 </div>
 
 
