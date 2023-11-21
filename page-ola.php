396a91cadd26c492c5001e10398428f2a2aaaf79
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-21 11:19:06 -0500

    :sparkles: Update classnaming

--- page-ola.php
+++ page-ola.php
@@ -1,10 +1,14 @@
 <?php set_query_var( 'ENTRY', 'ola' );
 get_header(); ?>
 
-<div>
-    <img src="<?php echo get_stylesheet_directory_uri() . '/uploads/OLA_BANNER_MAIN.webp' ?>" alt="skullcandy-ola"/>
-    <img src="<?php echo get_stylesheet_directory_uri() . '/uploads/OLA_LOGO.png' ?>"  />
-</div>
+
+<main class="main">
+    <div class="main-banner">
+        <img class="main-banner__bg" src="<?php echo get_stylesheet_directory_uri() . '/uploads/OLA_BANNER_MAIN.webp' ?>" alt="skullcandy-ola"/>
+        <img class="main-banner__logo" src="<?php echo get_stylesheet_directory_uri() . '/uploads/OLA_LOGO.png' ?>"  />
+    </div>
+</main>
+
 
 
 <?php get_footer();
