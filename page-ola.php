e93e7e7fc39e85275b00440fe7a76627151d2d31
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-21 11:18:02 -0500

    :sparkles: Update logo ola

--- page-ola.php
+++ page-ola.php
@@ -3,6 +3,7 @@ get_header(); ?>
 
 <div>
     <img src="<?php echo get_stylesheet_directory_uri() . '/uploads/OLA_BANNER_MAIN.webp' ?>" alt="skullcandy-ola"/>
+    <img src="<?php echo get_stylesheet_directory_uri() . '/uploads/OLA_LOGO.png' ?>"  />
 </div>
 
 
