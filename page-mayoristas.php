30d03ed6cfaf5df0d3fde5717bad3c185e39b28b
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-21 15:48:25 -0500

    :sparkles: Update name

--- page-mayoristas.php
+++ page-mayoristas.php
@@ -8,7 +8,7 @@ get_header(); ?>
             Gracias por tu interés en comprar Skullcandy para tu negocio. Por favor, ingresa tus datos en el formulario y nosotros nos contactaremos contigo. 
         </p>
         <div class="forms-mayoristas">
-            <?php echo do_shortcode( '[contact-form-7 id="178412" title="La Ola del Año"]' ); ?>
+            <?php echo do_shortcode( '[contact-form-7 id="178412" title="Ola SKDY"]' ); ?>
         </div>
         <div class="forms-adicional">
             <p class="forms-adicional__content"> Si deseas contactarte por otro medio, por favor comunícate con nuestro canal de atención al cliente <a href="https://www.wa.link/xfavry/">aquí</a>. También puedes enviarnos un correo a atencioncliente@blacksheep.com.pe</p>
