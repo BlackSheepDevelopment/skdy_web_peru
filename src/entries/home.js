79b447847b56c6bc09b42361314f4d037c0a2790
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-23 17:15:19 -0500

    :lipstick: Update home values with color

--- src/entries/home.js
+++ src/entries/home.js
@@ -133,15 +133,15 @@ $(document).ready(function () {
         document.getElementById("timer").innerHTML =
             "<div class='timer__item'><div class='timer__item__number'>" +
             Math.floor(distance / (1000 * 60 * 60 * 24)) +
-            "</div><div class='timer__item__text'>days</div></div>" +
+            "</div><div class='timer__item__text'>DÍAS</div></div>" +
             "<div class='timer__item'><div class='timer__item__number'>" +
             Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) +
-            "</div><div class='timer__item__text'>hours</div></div>" +
+            "</div><div class='timer__item__text'>HORAS</div></div>" +
             "<div class='timer__item'><div class='timer__item__number'>" +
             Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) +
-            "</div><div class='timer__item__text'>minutes</div></div>" +
+            "</div><div class='timer__item__text'>MINUTOS</div></div>" +
             "<div class='timer__item'><div class='timer__item__number'>" +
             Math.floor((distance % (1000 * 60)) / 1000) +
-            "</div><div class='timer__item__text'>seconds</div></div>";
+            "</div><div class='timer__item__text'>SEGUNDOS</div></div>";
     }, 1000);
 }, false);
