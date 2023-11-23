73b37afd8fa3044aab2a7b75d54949e7f502ac97
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-23 16:27:31 -0500

    :lipstick: Update ofertas

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -48,7 +48,7 @@ do_action( 'woocommerce_before_main_content' );
 
 <div class="main-shop__banner">
 	<img class="main-shop__img" src="<?php echo get_stylesheet_directory_uri() . '/uploads/BF_BANNER_SHOP.png' ?>"/>
-	<p class="main-shop__title">Vive la música con nuestras ofertas></p>
+	<p class="main-shop__title">Aprovecha el BLACK FRIDAY en Skullcandy</p>
 </div>
 
 
