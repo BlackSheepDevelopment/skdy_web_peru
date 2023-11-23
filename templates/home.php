4b378e7d86f549a4d326496803961baa6ad6362f
elmerescandon <elmer.escandon@utec.edu.pe>
2023-11-23 15:24:07 -0500

    :sparkles: Update home values

--- templates/home.php
+++ templates/home.php
@@ -51,8 +51,8 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
             <div class="home-picture" id="skullcandy-peru-container">               
                 <div>
                     <picture class="home-grid__video"
-                            <source srcset="<?php echo get_stylesheet_directory_uri() . '/uploads/BANNER_WEB.jpg' ?>" media="(min-width: 758px)"/>
-                            <img src="<?php echo get_stylesheet_directory_uri() . '/uploads/BANNER_WEB_MOV.jpg' ?>"
+                            <source srcset="<?php echo get_stylesheet_directory_uri() . '/uploads/BANNER_WEB_MOV.jpg' ?>" media="(min-width: 758px)"/>
+                            <img src="<?php echo get_stylesheet_directory_uri() . '/uploads/BANNER_WEB.jpg' ?>"
                                     alt="mobile-main-banner">
                     </picture>
                     <!-- <picture class="home-grid__video">
