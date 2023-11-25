37091c74b6dce95649a6d11d212dd3163253c302
Raul Escandon <elmer.escandon@utec.edu.pe>
2023-11-24 23:19:31 -0500

    Add countdown timer and hide element if distance is less than 0

--- src/entries/shop.js
+++ src/entries/shop.js
@@ -62,19 +62,6 @@ $(".filter__section__button").on("click", function (event) {
     }
 });
 
-// filter-categories__options
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Add countdown timer and hide element if distance
-=======
->>>>>>> Add countdown timer and hide element if distance
-=======
->>>>>>> Add countdown timer and hide element if distance
-
 // post-171520
 // 173942
 // $(document).ready(function () {
@@ -88,40 +75,3 @@ $(".filter__section__button").on("click", function (event) {
 //         $(".post-173942").hide();
 //     }
 // }, false);
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Add countdown timer and hide element if distance
-=======
->>>>>>> Add countdown timer and hide element if distance
-=======
->>>>>>> Add countdown timer and hide element if distance
-=======
->>>>>>> Add countdown timer and hide element if distance
-$(document).ready(function () {
-    // create a countdown timer and create div containers inside a div container with id timer
-    var countDownDate = new Date("Nov 24, 2023 23:00:00").getTime();
-    // If the current hour more than 5, hide an element with class post-171520 and post-173942
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    if (distance < 0) {
-        $(".post-162068").hide();
-    }
-}, false);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> Add countdown timer and hide element if distance
-=======
-=======
->>>>>>> Add countdown timer and hide element if distance
-=======
->>>>>>> Add countdown timer and hide element if distance
-=======
->>>>>>> Add countdown timer and hide element if distance
-=======
->>>>>>> Add countdown timer and hide element if distance
->>>>>>> Add countdown timer and hide element if distance
