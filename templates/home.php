6a78cc774c32cb9a773684af95b5e1a7bd24600c
elmerescandon <elmer.escandon@utec.edu.pe>
2023-12-13 13:14:29 -0500

    :sparkles: Change home

--- templates/home.php
+++ templates/home.php
@@ -80,10 +80,10 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                     </div> -->
                 </div>
 
-                <div>
+                <!-- <div>
                     <picture class="home-grid__video">
-                            <source srcset="<?php echo get_stylesheet_directory_uri() . '/uploads/JIB_BANNER_DESK.png ' ?>" media="(min-width: 1280px)"/>
-                            <img src="<?php echo get_stylesheet_directory_uri() . '/uploads/JIB_BANNER_MOV.png' ?>"
+                            <source srcset="<?php #echo get_stylesheet_directory_uri() . '/uploads/JIB_BANNER_DESK.png ' ?>" media="(min-width: 1280px)"/>
+                            <img src="<?php #echo get_stylesheet_directory_uri() . '/uploads/JIB_BANNER_MOV.png' ?>"
                                     alt="mobile-main-banner">
                     </picture>
 
@@ -96,7 +96,7 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
                             </div>
                     </div>
 
-                </div>
+                </div> -->
 
                 <!-- <div>
                     <picture class="home-grid__video">
