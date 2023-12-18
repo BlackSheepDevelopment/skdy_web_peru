853a025805bb287e93699ce2ba4a87764bd87a6f
elmerescandon <elmer.escandon@utec.edu.pe>
2023-12-18 12:13:58 -0500

    Update delivery time message

--- page-checkout.php
+++ page-checkout.php
@@ -27,7 +27,7 @@ get_header(); ?>
 				<div class="shop-features__container">
 					<svg width="75" height="75" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16"> <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/> <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/> </svg>
 					<div class="shop-features__container__info">
-						<p class="shop-features__container__info__title">Cómpralo antes de las 4:00 PM y te lo entregamos al día siguiente (Lima Metropolitana y Callao, solo Lunes a Jueves) </p>
+						<p class="shop-features__container__info__title">Tiempo de entrega entre 2 a 4 días hábiles.</p>
 					</div>
 					
 				</div>
