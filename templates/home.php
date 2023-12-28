3bd416eb3701a7c3392bfad9628a3b459a72bb63
elmerescandon <elmer.escandon@utec.edu.pe>
2023-12-28 14:03:13 -0500

    Add banner titles to home.php template

--- templates/home.php
+++ templates/home.php
@@ -53,12 +53,11 @@ if ( $shop_notice['show'] && $shop_notice['text'] ): ?>
             <div class="home-picture" id="skullcandy-peru-container">     
                 <?php 
                     if (have_rows("banners_grid")){
+                        ?> <div> Hola como estas quiero probar algo</div>  <?php 
                         while( have_rows('repeater_field_name') ){
                             the_row();
                             $sub_value = get_sub_field('title');
-                            ?> 
-                            <div> <?php echo $sub_value ?> </div>
-                            <?php 
+                            ?> <div> <?php echo $sub_value ?> </div>  <?php 
                         } 
                     }
                 ?>    
